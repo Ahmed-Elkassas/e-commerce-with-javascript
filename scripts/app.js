94a82176e738084ccd7d9c2f9c98db1db3540386
Ahmed-Basuony <ahmed.basouny@awstreams.com>
2022-04-19 22:42:17 +0200

    Display products to the ui dynamically with js

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,6 @@
 // Main classes
 
-class productList {
+class ProductList {
   async getProduct() {
     try {
       let result = await fetch("../products.json");
@@ -13,11 +13,49 @@ class productList {
   }
 }
 
-class UI {}
+const productDOM = document.querySelector(".products__center");
+class UI {
+  displayProducts(products) {
+    let uiResult = "";
+    products.forEach((product) => {
+      uiResult += `
+          <article class="product">
+            <div class="product__container">
+              <img
+                src="${product.imageUrl}"
+                alt="${product.title}"
+                class="product__container--img"
+              />
+              <button class="product__container--btn" data-id=${product.id}>
+                <i class="fa-solid fa-basket-shopping-simple"></i>
+                add to cart
+              </button>
+            </div>
+            <h4 class="product__name">${product.title}</h4>
+            <h5 class="product__price">$${product.price}</h5>
+          </article>
+      `;
+      productDOM.innerHTML = uiResult;
+    });
+  }
+}
 
 class Storage {}
 
-document.addEventListener("DOMContentLoaded", () => {
-  const product = new productList();
-  product.getProduct().then((data) => console.log(data));
-});
+class App {
+  static init() {
+    const ui = new UI();
+    const products = new ProductList();
+    products.getProduct().then((products) => ui.displayProducts(products));
+  }
+}
+App.init();
+
+// Another approach to display to the UI ============== 👇
+
+// document.addEventListener("DOMContentLoaded", () => {
+//   const ui = new UI();
+//   const products = new ProductList();
+//   products.getProduct().then((products) => ui.displayProducts(products));
+//   //   products.getProduct().then((data) => console.log(data));
+// });
