a7b7b3c38fd29d84265263ecff9a49ed1d526cbc
Ahmed-Basuony <ahmed.basouny@awstreams.com>
2022-04-19 23:53:57 +0200

    Deal with cartButtons and add product to the local storage as in cart array

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,4 @@
+let cart = [];
 // Main classes
 
 class ProductList {
@@ -38,11 +39,51 @@ class UI {
       productDOM.innerHTML = uiResult;
     });
   }
+  getCartButtons() {
+    let buttons = document.querySelectorAll(".product__container--btn");
+    let buttonsArray = [...buttons];
+    // console.log(buttonsArray);
+    buttonsArray.forEach((button) => {
+      let buttonId = button.dataset.id;
+      let elementInCart = cart.find((element) => element.id === buttonId);
+      /**
+       * change button textContent
+       * make button disabled
+       */
+      if (elementInCart) {
+        button.textContent = "Checkout";
+        button.disabled = true;
+      }
+      button.addEventListener("click", (event) => {
+        event.target.textContent = "Checkout";
+        event.target.disabled = true;
+        /**
+         * [1] Get product from productList
+         * [2] Add product to Cart array
+         * [3] Save cart to the LocalStorage
+         * [4] set cart value
+         * [5] display cart item
+         * [6] show the cart section
+         */
+        // add amount property because we will use it in cart section
+        let cartItem = { ...Storage.getProduct(buttonId), amount: 1 };
+        cart = [...cart, cartItem];
+        Storage.saveCart(cart);
+      });
+    });
+  }
 }
 
 class Storage {
   static saveProducts(products) {
     localStorage.setItem("products", JSON.stringify(products));
+  }
+  static getProduct(id) {
+    let products = JSON.parse(localStorage.getItem("products"));
+    return products.find((product) => product.id === id);
+  }
+  static saveCart(cart) {
+    localStorage.setItem("cart", JSON.stringify(cart));
   }
 }
 
@@ -50,10 +91,14 @@ class App {
   static init() {
     const ui = new UI();
     const products = new ProductList();
-    products.getProduct().then((products) => {
-      ui.displayProducts(products);
-      Storage.saveProducts(products);
-    });
+    products
+      .getProduct()
+      .then((products) => {
+        ui.displayProducts(products);
+        Storage.saveProducts(products);
+      })
+      // add here to can access buttons after html code been loaded
+      .then(() => ui.getCartButtons());
   }
 }
 App.init();
