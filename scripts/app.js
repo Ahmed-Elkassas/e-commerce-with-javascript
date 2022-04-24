5a88cbffb445e8f873e0a91f5612c8c068cff7f2
Ahmed-Elkassas <ahmedelkassas2811@gmail.com>
2022-04-25 01:49:53 +0200

    Add setUpAPP funtion to start app with cart data which storage in localStorage

--- scripts/app.js
+++ scripts/app.js
@@ -146,6 +146,11 @@ class Storage {
   static saveCart(cart) {
     localStorage.setItem("cart", JSON.stringify(cart));
   }
+  static getCart() {
+    return localStorage.getItem("cart")
+      ? JSON.parse(localStorage.getItem("cart"))
+      : [];
+  }
 }
 
 class App {
